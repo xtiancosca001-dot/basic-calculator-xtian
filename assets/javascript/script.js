d85608d83f06b81d10b5f6ddfa6b6927941f0641
Christian Eric Cosca <ccosca@iripple.com>
2025-07-21 17:17:59 +0800

    Impelement basic calculator functions

---
+++ assets/javascript/script.js
@@ -0,0 +1,32 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function percent(num) {
+    return num * 100;
+}
+
+function operate(operator, num1, num2) {
+    switch(operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case 'x':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+    }
+}
